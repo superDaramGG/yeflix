284777fba7de39817e91d77568bfae1e966280c1
Yeseo <superior817@naver.com>
2020-07-14 16:03:24 +0900

    예플릭스 추가

---
+++ README.md
@@ -0,0 +1,19 @@
+# yeflix
+
+## Screens
+
+- [] Home
+- [] TV Shows
+- [] Search
+- [] Detail
+
+## API Verbs
+
+- [x] Now playing (Movie)
+- [x] Upcoming (Movie)
+- [x] Top Rated (TV, Movie)
+- [x] Popular (TV, Movie)
+- [x] Airing Today (TV)
+- [x] TV Show Detail
+- [x] Movie Detail
+- [] Search (Movie, TV)
---
+++ src/Routes/Search/SearchPresenter.js
@@ -0,0 +1,107 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import Helmet from "react-helmet";
+import Loader from "Components/Loader";
+import Section from "Components/Section";
+import Message from "../../Components/Message";
+import Poster from "../../Components/Poster";
+
+const Container = styled.div`
+  padding: 0 20px;
+  padding: 20px;
+`;
+
+const Form = styled.form`
+  margin-bottom: 50px;
+  width: 100%;
+`;
+
+const Input = styled.input`
+  all: unset;
+  font-size: 28px;
+  width: 100%;
+`;
+
+const SearchPresenter = ({
+  movieResults,
+  tvResults,
+  loading,
+  searchTerm,
+  handleSumbit,
+  error,
+  updateTerm,
+}) => (
+  <Container>
+    <Helmet>
+      <title>Search | Yeflix</title>
+    </Helmet>
+    <Form onSubmit={handleSumbit}>
+      <Input
+        placeholder="Search Movies or TV Shows..."
+        value={searchTerm}
+        onChange={updateTerm}
+      />
+    </Form>
+    {loading ? (
+      <Loader />
+    ) : (
+      <>
+        {movieResults && movieResults.length > 0 && (
+          <Section title="Movie Results">
+            {movieResults.map((movie) => (
+              <>
+                <span key={movie.id}>{movie.title}</span>
+                <Poster
+                  key={movie.id}
+                  id={movie.id}
+                  imageUrl={movie.poster_path}
+                  title={movie.original_title}
+                  rating={movie.vote_average}
+                  year={movie.release_date.substring(0, 4)}
+                  isMovie={true}
+                />
+              </>
+            ))}
+          </Section>
+        )}
+        {tvResults && tvResults.length > 0 && (
+          <Section title="Movie Results">
+            {tvResults.map((show) => (
+              <>
+                <span key={show.id}>{show.title}</span>
+                <Poster
+                  key={show.id}
+                  id={show.id}
+                  imageUrl={show.poster_path}
+                  title={show.original_title}
+                  rating={show.vote_average}
+                  year={show.release_date.substring(0, 4)}
+                />
+              </>
+            ))}
+          </Section>
+        )}
+        {error && <Message color="#e74c3c" text={error} />}
+        {tvResults &&
+          movieResults &&
+          tvResults.length === 0 &&
+          movieResults.length === 0 && (
+            <Message text="Nothing found" color="#95a5a6" />
+          )}
+      </>
+    )}
+  </Container>
+);
+
+SearchPresenter.propTypes = {
+  movieResults: PropTypes.array,
+  tvResults: PropTypes.array,
+  error: PropTypes.string,
+  searchTerm: PropTypes.string,
+  loading: PropTypes.bool.isRequired,
+  handleSumbit: PropTypes.func.isRequired,
+  updateTerm: PropTypes.func.isRequired,
+};
+
+export default SearchPresenter;
